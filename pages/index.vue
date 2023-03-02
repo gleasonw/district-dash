c26b085da4b75c1146b99c8793b13f1142d81fb9
Will Gleason <wgleason9@gmail.com>
2023-03-02 09:45:43 -0800

    trpc and api route

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp();
+const test = await $client.hello.useQuery({ text: "test" });
+const selected_district = ref(0);
+const districtInput = ref("");
+
+watch((districtInput), async () => {
+    console.log(districtInput.value);
+    console.log(districtInput.value);
+    const { data } = await $client.hello.useQuery({ text: districtInput.value });
+    console.log(data);
+});
+
+
+watch(districtInput, async () => {
+});
+</script>
+
+<template>
+    <div class="flex flex-col gap-5 align-center justify-center m-10">
+        <h1 class="text-center p-10 text-3xl">Search for a school district</h1>
+        <input v-model="districtInput" class="border-black border-2 border-b-4 p-3 rounded-lg" />
+        <div class="h-96 border-black border-2 border-b-4 p-5 rounded-lg">
+            Data!
+            {{ districtInput }}
+        </div>
+        <input class="border-black border-2 border-b-4 p-3 rounded-lg" />
+    </div>
+</template>
---
+++ server/api/districts.ts
@@ -0,0 +1,17 @@
+import Client from "pg/lib/client.js";
+
+export default defineEventHandler(async (event) => {
+  const client = new Client();
+  const query = getQuery(event);
+  console.log(query.name)
+  await client.connect();
+  const { rows } = await client.query(
+    `SELECT * FROM district WHERE lower("LEA_NAME") LIKE '%' || lower($1) || '%' LIMIT 10`,
+    [query.name]
+  );
+  await client.end();
+
+  return {
+    rows,
+  };
+});
