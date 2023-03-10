d6c42eef2f2da6f8ffde18294464469d98a4ea1a
Will Gleason <wgleason9@gmail.com>
2023-03-10 11:49:05 -0800

    nav bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { District, School } from '~~/models';
 
-const selected_district = ref(0);
+const selectedDistrict = ref<District>();
 const districtInput = ref("");
 const schoolInput = ref("");
 const currentFadeIndex = ref(0);
@@ -37,7 +37,7 @@ const showSchools = computed(() => {
 });
 
 const handleDistrictClick = (district: District) => {
-    selected_district.value = district.OBJECTID;
+    selectedDistrict.value = district;
     districtInput.value = "";
 };
 
@@ -60,33 +60,39 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="flex flex-col gap-5 align-center justify-center m-10">
-        <h1 class="text-center p-10 text-3xl">Search for a school district</h1>
-        <div v-if="!showDistricts" class="flex flex-row justify-center items-center gap-2">
-            <Transition name="slide-fade" mode="out-in">
-                <p :key="currentFadeIndex">{{ districtsForFade && districtsForFade[currentFadeIndex].LEA_NAME }}</p>
-            </Transition>
-        </div>
-        <div class="relative">
-            <input v-model="districtInput" class="border-black border-2 border-b-4 p-3 rounded-lg w-full" />
-        </div>
-        <div class="h-96 border-black border-2 border-b-4 p-5 rounded-lg">
-            <div v-for="district in districts" :key="district.OBJECTID">
-                <button @click=handleDistrictClick(district) class="hover:bg-zinc-100">
-                    {{ district.LEA_NAME }}
-                </button>
+    <BaseLayout>
+        <div class="flex flex-col gap-5 align-center justify-center m-10">
+            <h1 class="text-center p-10 text-3xl">Search for a school district</h1>
+            <div class="flex flex-row justify-center items-center gap-2">
+                <Transition name="slide-fade" mode="out-in">
+                    <p :key="currentFadeIndex">{{ districtsForFade && districtsForFade[currentFadeIndex].LEA_NAME }}</p>
+                </Transition>
+            </div>
+            <div class="relative">
+                <input v-model="districtInput" class="border-black border-2 border-b-4 p-3 rounded-lg w-full" />
+                <div v-if="showDistricts"
+                    class="absolute z-40 bg-white rounded-sm border-l border-r border-b pl-2 pr-2 pb-2 w-full">
+                    <div v-for="district in districts" :key="district.OBJECTID">
+                        <button @click=handleDistrictClick(district) class="hover:bg-zinc-100">
+                            {{ district.LEA_NAME }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="h-96 border-black border-2 border-b-4 p-5 rounded-lg">
+                {{ selectedDistrict }}
+            </div>
+            <div class="relative">
+                <input v-model="schoolInput" class="border-black border-2 border-b-4 p-3 rounded-lg w-full" />
+                <div v-if="showSchools"
+                    class="absolute z-40 bg-white rounded-sm border-l border-r border-b pl-2 pr-2 pb-2 w-full">
+                    <ul v-for="school in schools" :key="school.OBJECTID">
+                        {{ school.SCH_NAME }}
+                    </ul>
+                </div>
             </div>
         </div>
-        <div class="relative">
-            <input v-model="schoolInput" class="border-black border-2 border-b-4 p-3 rounded-lg w-full" />
-            <div v-if="showSchools"
-                class="absolute z-40 bg-white rounded-sm border-l border-r border-b pl-2 pr-2 pb-2 w-full">
-                <ul v-for="school in schools" :key="school.OBJECTID">
-                    {{ school.SCH_NAME }}
-                </ul>
-            </div>
-        </div>
-    </div>
+    </BaseLayout>
 </template>
 
 <style>
@@ -99,12 +105,10 @@ onMounted(() => {
 }
 
 .slide-fade-enter-from {
-    transform: translateX(20px);
     opacity: 0;
 }
 
 .slide-fade-leave-to {
-    transform: translateX(-20px);
     opacity: 0;
 }
 </style>
